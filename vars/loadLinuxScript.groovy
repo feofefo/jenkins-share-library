892ae59c66e791c15df56b491a96844bafbc1d77
wobushilaowu <laowudeemail@163.com>
2023-05-31 23:33:57 +0800

    ssss

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
-def call(Map config = [:]) {
-    def script = libraryResource "${config.name}"
-    writeFile file: "${config.name}", text: script
-    sh "chmod +x ${config.name}"
+def call(String name) {
+    def script = libraryResource "${name}"
+    writeFile file: "${name}", text: script
+    sh "chmod +x ${name}"
 }
